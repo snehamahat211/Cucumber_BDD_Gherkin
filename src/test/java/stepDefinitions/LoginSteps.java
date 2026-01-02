c3f585afb3a827fc775ad978a6ee7865c5f10de0
snehamahat211 <snehamahat211@gmail.com>
2026-01-02 16:23:33 +0545

    file changed

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -17,7 +17,7 @@ public class LoginSteps {
     @Given("I am in the login page of the para Bank Application")
     public void i_am_in_the_login_page_of_the_para_bank_application() {
         driver = new ChromeDriver();
-        // INITIALIZE WAIT HERE:
+
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         driver.manage().window().maximize();
         driver.get("https://parabank.parasoft.com/parabank/index.htm");
@@ -26,19 +26,19 @@ public class LoginSteps {
     @When("I enter valid credential")
     public void i_enter_valid_credential() {
         driver.findElement(By.name("username")).sendKeys("snehaa");
-        driver.findElement(By.name("password")).sendKeys("confirm"); // Use your real password
+        driver.findElement(By.name("password")).sendKeys("confirm");
         driver.findElement(By.xpath("//input[@value='Log In']")).click();
     }
 
     @Then("I should be taken to the Overview page")
     public void i_should_be_taken_to_the_overview_page() {
         try {
-            // This will now work because 'wait' is no longer null
+
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Log Out")));
             System.out.println("Login Successful!");
             driver.findElement(By.linkText("Log Out")).click();
         } catch (TimeoutException e) {
-            // This captures the error message from the website UI if login fails
+
             String errorMsg = driver.findElement(By.className("error")).getText();
             System.out.println("Login Failed! Site says: " + errorMsg);
             throw e;
--- src/test/java/stepDefinitions/TestRunner.java
+++ src/test/java/stepDefinitions/TestRunner.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        // If your folder is actually named "Features", change it here:
         features = "src/test/resources/login.feature",
         glue = "stepDefinitions",
         plugin = {"pretty"}
